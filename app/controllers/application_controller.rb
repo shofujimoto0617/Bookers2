8d91f206af509c6ebc9469369531177202f44d4b
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-18 16:20:52 +0900

    [Update]ログイン状態に合わせて、リンクを設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def after_sign_in_path_for(resource)
+  	books_path
+  end
+
+  def after_sign_out_path_for(resouce)
+  	new_user_session_path
+  end
+
 end
