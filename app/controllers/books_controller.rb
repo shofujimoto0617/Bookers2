a99d64f79ba4f20e24147562d8db3235da86e318
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-18 12:37:54 +0900

    [add]投稿画面(new,create)追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
 
-  def new
+  def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def create
-  end
-
-  def index
+  	@book = Book.new(book_params)
+  	@book.save
+  	redirect_to books_path
   end
 
   def show
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 
 end
