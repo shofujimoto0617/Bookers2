13f3fc808839ae962759c879abe2b9603e0ffef2
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-18 11:44:13 +0900

    [add]Bookモデル追加→一覧画面(index)設置

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,15 @@
+class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  root 'books#index'
+  resources :books, only: [:new, :create, :index, :show]
 
 end
