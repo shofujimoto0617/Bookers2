b7ec5b2b6205889b4292dda73d761b3846705b04
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-19 16:31:46 +0900

    [Update]Bookモデルのindexにプロフィール設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def index
   	@books = Book.all
   	@book = Book.new
+  	@user = User.find(current_user.id)
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :introduction, length: { maximum: 200 }
+  validates :introduction, length: { maximum: 50 }
   has_many :books, dependent: :destroy
   attachment :profile_image
 end
