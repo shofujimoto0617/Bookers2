bc56473ffa4a7f261d7d1ef3d696a59636e2f4a1
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-19 12:37:20 +0900

    [add]マイページ編集画面設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :introduction, length: { maximum: 200 }
   has_many :books, dependent: :destroy
 end
