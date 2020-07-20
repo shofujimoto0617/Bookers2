0af84021df9a0873b805fe01e62dd8bb8d30c3ab
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-20 13:45:39 +0900

    [Update]Bookモデルの新規投稿後詳細画面にリダイレクト、[add]Bookモデルにedit追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,22 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	@book.user_id = current_user.id
   	@book.save
-  	redirect_to books_path
+  	redirect_to book_path(@book.id)
   end
 
   def show
   	@book = Book.find(params[:id])
   	@user = User.find(current_user.id)
-  	@book = Book.new
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to book_path(@book)
   end
 
   private
